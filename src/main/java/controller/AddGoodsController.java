67917d9a91c30643225a778cb3fff6920cb71a3e
YoungwooJ <cyw960714@naver.com>
2023-01-05 12:22:21 +0900

    Goods CRUD 생성

---
+++ src/main/java/controller/AddGoodsController.java
@@ -0,0 +1,90 @@
+package controller;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+import service.GoodsService;
+import vo.Goods;
+import vo.GoodsImg;
+
+@WebServlet("/AddGoodsController")
+public class AddGoodsController extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// View
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/view/goods/addGoods.jsp");
+		
+		rd.forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 프로젝트안 upload폴더의 실제 물리적 위치를 반환
+		String dir = request.getServletContext().getRealPath("/upload");
+		int maxFileSize = 1024 * 1024 * 100; // 100Mbyte
+		
+		// 업로드 폴더내 동일한 이름이 있으면 뒤에 숫자를 추가
+		DefaultFileRenamePolicy fp = new DefaultFileRenamePolicy();
+		MultipartRequest mreq = new MultipartRequest(request, dir, maxFileSize, "UTF-8", fp);
+		
+		String goodsName = mreq.getParameter("goodsName");
+		int goodsPrice = Integer.parseInt(mreq.getParameter("goodsPrice"));
+		int goodsStock = Integer.parseInt(mreq.getParameter("goodsStock"));
+		String empId = mreq.getParameter("empId");
+		int  hit = Integer.parseInt(mreq.getParameter("hit"));
+		
+		// input type=file 바이너리 파일은 마임타입형태의 파일로 변환되어 upload폴더의 자동으로 저장
+		String contentType = mreq.getContentType("goodsImg");
+		String originalFileName = mreq.getOriginalFileName("goodsImg"); // 원본 파일 이름
+		String fileSystemName = mreq.getFilesystemName("goodsImg"); // 저장된 파일 이름
+		
+		// 이미지 파일 검사
+		if(contentType.equals("image/jpeg") || contentType.equals("image/png")) {
+			Goods goods = new Goods();
+			GoodsImg goodsImg = new GoodsImg();
+			
+			goods.setGoodsName(goodsName);
+			goods.setGoodsPrice(goodsPrice);
+			goods.setGoodsStock(goodsStock);
+			goods.setEmpId(empId);
+			goods.setHit(hit);
+			
+			goodsImg.setFileName(fileSystemName);
+			goodsImg.setOriginName(originalFileName);
+			goodsImg.setContentType(contentType);
+			
+			GoodsService goodsService = new GoodsService();
+			goodsService.addGoods(goods, goodsImg, dir);
+			
+			// 디버깅 코드
+			System.out.println("문자열 매개값 : ");
+			System.out.println("상품 이름 : " + goodsName);
+			System.out.println("상품 가격 : " + goodsPrice);
+			System.out.println("상품 재고 : " + goodsStock);
+			System.out.println("직원 아이디 : " + empId);
+			System.out.println("히트 : " + hit);
+			
+			System.out.println("파일 매개값 : ");
+			System.out.println("파일이름 : " + fileSystemName);
+			System.out.println("원본 파일 이름 : " + originalFileName);
+			System.out.println("파일 컨텐츠 타입 : " + contentType);
+		} else {
+			System.out.println("*.jpg, *.png파일만 업로드 가능");
+			File f = new File(dir+"\\"+mreq.getFilesystemName("itemImg"));
+			if(f.exists()) {
+				f.delete(); // 이미지가 아닌 파일이 업로드 되었기때문에 삭제
+			}
+		}
+		
+		// View
+		response.sendRedirect(request.getContextPath()+"/GoodsListController");
+	}
+}
---
+++ src/main/java/dao/GoodsDao.java
@@ -0,0 +1,86 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import util.DBUtil;
+import vo.Goods;
+
+public class GoodsDao {
+	// SELECT
+	// 상품 리스트
+	public ArrayList<HashMap<String, Object>> selectgoodsList(Connection conn) throws Exception {
+		ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String,Object>>();
+		String sql = "SELECT g.goods_code goodsCode, g.goods_name goodsName, img.filename filename"
+				   + "	FROM goods g INNER JOIN goods_img img"
+				   + "	ON g.goods_code = img.goods_code";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		ResultSet rs = stmt.executeQuery();
+		while(rs.next()) {
+			HashMap<String, Object> m = new HashMap<String, Object>();
+			m.put("goodsCode", rs.getInt("goodsCode"));
+			m.put("goodsName", rs.getString("goodsName"));
+			m.put("filename", rs.getString("filename"));
+			list.add(m);
+		}
+		DBUtil.close(rs, stmt, null);
+		
+		return list;
+	}
+	
+	// 상품 상세페이지
+	public HashMap<String, Object> selectgoodsOne(Connection conn, int goodsCode) throws Exception {
+		HashMap<String, Object> map = null;
+		String sql = "SELECT g.goods_code goodsCode, g.goods_name goodsName, g.goods_price goodsPrice, g.goods_stock goodsStock, g.emp_id empId, g.hit hit, g.createdate createdate, img.filename filename, img.origin_name originName"
+				   + "	FROM goods g INNER JOIN goods_img img"
+				   + "	ON g.goods_code = img.goods_code"
+				   + "	WHERE  g.goods_code = ?";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, goodsCode);
+		ResultSet rs = stmt.executeQuery();
+		while(rs.next()) {
+			map = new HashMap<String, Object>();
+			map.put("goodsCode", rs.getInt("goodsCode"));
+			map.put("goodsName", rs.getString("goodsName"));
+			map.put("goodsPrice", rs.getInt("goodsPrice"));
+			map.put("goodsStock", rs.getInt("goodsStock"));
+			map.put("empId", rs.getString("empId"));
+			map.put("hit", rs.getInt("hit"));
+			map.put("createdate", rs.getString("createdate"));
+			map.put("filename", rs.getString("filename"));
+			map.put("originName", rs.getString("originName"));
+		}
+		DBUtil.close(rs, stmt, null);
+		
+		return map;
+	}
+	
+	// INSERT
+	public HashMap<String, Integer> insertGoods(Connection conn, Goods goods) throws Exception {
+			String sql = "INSERT INTO goods(goods_name, goods_price, goods_stock, emp_id, hit, createdate) VALUES(?, ?, ?, ?, ?, NOW())";
+			// PreparedStatement.RETURN_GENERATED_KEYS 쿼리실행 후 생성된 auto_increment값을 ResultSet에 반환
+			PreparedStatement stmt = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+			stmt.setString(1, goods.getGoodsName());
+			stmt.setInt(2, goods.getGoodsPrice());
+			stmt.setInt(3, goods.getGoodsStock());
+			stmt.setString(4, goods.getEmpId());
+			stmt.setInt(5, goods.getHit());
+			int row = stmt.executeUpdate();
+			ResultSet rs = stmt.getGeneratedKeys();
+			int autoKey = 0;
+			if(rs.next()) {
+				autoKey = rs.getInt(1); // stmt.executeUpdate(); 생성된 auto_increment값이 대입
+			}
+			
+			HashMap<String, Integer> map = new HashMap<String, Integer>();
+			map.put("row", row);
+			map.put("autoKey", autoKey);
+			
+			DBUtil.close(rs, stmt, null);
+			
+			return map;
+		}
+}
