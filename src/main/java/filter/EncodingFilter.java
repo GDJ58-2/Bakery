3ea27c43eb4eacab6e4702f1a38289e83ccaf7a8
YoungwooJ <cyw960714@naver.com>
2023-01-09 17:41:41 +0900

    [조영우] filter / listener 생성

---
+++ src/main/java/filter/EncodingFilter.java
@@ -0,0 +1,28 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+
+@WebFilter("/*")
+public class EncodingFilter extends HttpFilter implements Filter {
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// POST 요청만을 처리하고 싶다면...
+		if(request instanceof HttpServletRequest) {
+			HttpServletRequest req = (HttpServletRequest)request;
+			if(req.getMethod().equals("POST")) {
+				request.setCharacterEncoding("UTF-8");
+				System.out.println("인코딩 UTF-8 필터");
+			}
+		}
+		chain.doFilter(request, response);
+	}
+}
