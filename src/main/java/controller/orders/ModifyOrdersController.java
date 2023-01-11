80d04fcb9f8917e708f7a61a30e4fb1aaa4d1f7d
gaeun <mlee33444@naver.com>
2023-01-11 10:23:46 +0900

    [이가은] 주문수정 메서드 구현

--- src/main/java/controller/orders/ModifyOrdersController.java
+++ src/main/java/controller/orders/ModifyOrdersController.java
@@ -1,19 +1,55 @@
 package controller.orders;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ModifyOrders
- */
+import service.OrdersService;
+import vo.Orders;
+
 @WebServlet("/orders/modifyOrders")
 public class ModifyOrdersController extends HttpServlet {
+	private OrdersService ordersService;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getParameter("orderCode")==null||request.getParameter("orderCode").equals("")) {
+			response.sendRedirect(request.getContextPath()+"/orders/ordersList");
+			return;
+		}
+		int orderCode = Integer.parseInt(request.getParameter("orderCode"));
+		this.ordersService = new OrdersService();
+		HashMap<String, Object> map = ordersService.getOrdersOne(orderCode);
+		request.setAttribute("map", map);
+		request.getRequestDispatcher("/WEB-INF/view/orders/modifyOrders.jsp").forward(request, response);
+	}
 	// modify Orders action
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		String orderState = request.getParameter("orderState");
+		if(orderState==null||orderState.equals("")||request.getParameter("orderCode")==null||request.getParameter("orderCode").equals("")||request.getParameter("orderQuantity")==null||request.getParameter("orderQuantity").equals("")
+				||request.getParameter("orderPrice")==null||request.getParameter("orderPrice").equals("")) {
+			response.sendRedirect(request.getContextPath()+"/orders/ordersList");
+			return;
+		}
+		int orderQuantity = Integer.parseInt(request.getParameter("orderQuantity"));
+		int orderPrice = Integer.parseInt(request.getParameter("orderPrice"));
+		int orderCode = Integer.parseInt(request.getParameter("orderCode"));
+		
+		Orders orders = new Orders();
+		orders.setOrderCode(orderCode);
+		orders.setOrderQuantity(orderQuantity);
+		orders.setOrderPrice(orderPrice);
+		orders.setOrderState(orderState);
+		System.out.println(orders);
+		
+		this.ordersService = new OrdersService();
+		int row = ordersService.modifyOrdersByAdmin(orders);
+		System.out.println(row+"<--ModifyOrdersController row");
+		response.sendRedirect(request.getContextPath()+"/orders/ordersOne?orderCode="+orderCode);
 	}
 }
---
+++ src/main/webapp/WEB-INF/view/orders/modifyOrders.jsp
@@ -0,0 +1,97 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>modifyOrders</title>
+</head>
+<body>
+	<!-- 관리자페이지 -->
+	<h1>주문상세</h1>
+	<h3>주문 날짜 ${map.createdate} 주문 번호 ${map.orderCode}</h3>
+	<br>
+	<div style="color:red;">
+	${msg}
+	</div>
+	<form action="${pageContext.request.contextPath}/orders/modifyOrders" method="post">
+		<input type="hidden" name="orderCode" value="${map.orderCode}">
+		<table border="1">
+			<tr>
+				<th>상품 번호 : </th>
+				<td>${map.goodsCode}</td>
+			</tr>
+			<tr>
+				<th>상품 이미지 : </th>
+				<td>
+					<img src="${pageContext.request.contextPath}/upload/${map.filename}" width="400" height="400">
+				</td>
+			</tr>
+			<tr>
+				<th>상품 이름 : </th>
+				<td>${map.goodsName}</td>
+			</tr>
+			<tr>
+				<th>상품 종류 : </th>
+				<td>${map.categoryName}</td>
+			</tr>
+			<tr>
+				<th>상품 수량 : </th>
+				<td><input type="number" name="orderQuantity" value="${map.orderQuantity}">개</td>
+			</tr>
+			<tr>
+				<th>상품 가격 : </th>
+				<td><input type="number" name="orderPrice" value="${map.orderPrice}"></td>
+			</tr>
+			<tr>
+				<th>상품 배송상태 : </th>
+				<td><input type="text" name="orderState" value="${map.orderState}"></td> <!-- 배송, 교환, 환불 -->
+			</tr>
+			<tr>
+				<th>주문 날짜 : </th>
+				<td>${map.createdate}</td>
+			</tr>
+			<tr>
+				<th>총 가격 : </th>
+				<td>${map.orderPrice}</td>
+			</tr>
+			<tr>
+				<th>리뷰 작성 : </th> <!-- 작성된 리뷰 보기 -->
+				<td>
+					<a href="${pageContext.request.contextPath}/orders/modifyOrders?orderCode=${map.orderCode}">주문 수정</a>
+				</td>
+			</tr>
+			<tr>
+				<th>문의 : </th> <!-- 문의 보기  -->
+				<td>
+					<a href="${pageContext.request.contextPath}/question/addQuestion?orderCode=${map.orderCode}">주문 수정</a>
+				</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td> <!-- 구매확정 버튼 -->
+				<td><button type="submit">수정</button></td>
+			</tr>
+		</table>
+	</form>
+	
+	<h3>받는 사람 정보</h3>
+	<table>
+		<tr>
+			<th>고객 아이디 : </th>
+			<td>${map.customerId}</td>
+		</tr>
+		<tr>
+			<th>고객 이름 : </th>
+			<td>${map.customerName}</td>
+		</tr>
+		<tr>
+			<th>고객 전화번호 : </th>
+			<td>${map.customerPhone}</td>
+		</tr>
+		<tr>
+			<th>받는 주소 : </th>
+			<td>${map.address}</td>
+		</tr>
+	</table>
+</body>
+</html>
