bf008b1d4fb5aecf18ea54fdf261e9d90ef09b68
YoungwooJ <cyw960714@naver.com>
2023-01-31 16:59:02 +0900

    [조영우] index 페이지 생성 / index view, controller 추가

---
+++ src/main/java/controller/IndexController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/index")
+public class IndexController extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/view/index.jsp").forward(request, response);
+	}
+}
