a2ddfe4aeb10fa789d6f40544e4aa5a1667e279b
YoungwooJ <cyw960714@naver.com>
2023-02-01 17:30:16 +0900

    [조영우] 브랜드, 오시는 길, 블로그, 블로그 게시글, 강좌 페이지 추가

---
+++ src/main/java/controller/ContactController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/contact")
+public class ContactController extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/view/contact.jsp").forward(request, response);
+	}
+}
